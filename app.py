5e9ef1d14795b238b7fcdefed729a75cf1c54f15
Christelle Renaud <ellecccr@gmail.com>
2026-06-09 16:11:40 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 import flask
 from flask import Flask, render_template
 
@@ -44,4 +46,5 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port= int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
